8637d02297411199a317f4673738c1e4dfc0cb0f
kkkondae <zefdx2074@naver.com>
2024-07-01 00:25:55 +0900

    Update router.js

---
+++ router.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router();
+
+app.get('/list', (req, res)=>{
+
+})
+
+app.get('/write', (req, res)=>{
+
+})
+
+app.get('/view', (req, res)=>{
+
+})
+
+app.get('/update', (req, res)=>{
+
+})
+
+app.post('/update', (req, res)=>{
+
+})
+
+app.get('/delete', (req, res)=>{
+
+})
+
+module.exports = router;
